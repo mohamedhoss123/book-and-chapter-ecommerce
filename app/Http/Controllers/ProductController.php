3acd662db21e05dcee90a9983a188bcefa4df720
mohamedhoss123 <81392900+mohamedhoss123@users.noreply.github.com>
2025-02-10 16:40:15 +0200

    feat(product):Add pagination to product index page

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -13,7 +13,7 @@ class ProductController extends Controller
     public function index()
     {
         $products = Product::paginate(15);
-        return Inertia::render('Admin/Products/index', $products);
+        return Inertia::render('Admin/Products/index', ["products" => $products]);
     }
 
     public function create()
--- resources/js/Pages/Admin/Products/index.jsx
+++ resources/js/Pages/Admin/Products/index.jsx
@@ -1,14 +1,52 @@
 import { Button } from '@/components/ui/button'
 import AdminLayout from '@/Layouts/Admin.Layout'
 import { Link } from '@inertiajs/react'
-import React from 'react'
-export default function index() {
+import React, { useEffect } from 'react'
+import { Table } from '@/components/ui/table'
+
+
+export default function index({ products }) {
+    useEffect(() => {
+        console.log(products)
+    })
     return (
-        <AdminLayout> <div className='flex'>
-            <div className='flex w-full justify-end pr-20 mt-7'>
-                <Button><Link href="/products/create">add product +</Link></Button>
+        <AdminLayout>
+            <div className='flex flex-col'>
+                <div className='flex w-full justify-end pr-20 mt-7'>
+                    <Button><Link href="/products/create">add product +</Link></Button>
+                </div>
+                <div className='mt-4'>
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Price</th>
+                                <th>Stock</th>
+                                <th>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.data.map((product) => (
+                                <tr key={product.id}>
+                                    <td>{product.name}</td>
+                                    <td>${product.price}</td>
+                                    <td>{product.stock}</td>
+                                    <td>
+                                        <Button size="sm"><Link href={`/products/${product.id}/edit`}>Edit</Link></Button>
+                                        <Button size="sm" variant="danger">Delete</Button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </Table>
+                    <div className='mt-4 flex justify-center'>
+                        <Link href={products.first_page_url}>first</Link>
+
+                        <Link href={products.next_page_url}>next</Link>
+                    </div>
+                </div>
             </div>
-        </div>
         </AdminLayout>
     )
 }
+
